c907c1d16effcadb4d6fbcd35d66187335cba45c
Barry Smart <bsmart@hymans.co.uk>
2019-04-16 07:51:01 +0100

    Turning scratch pad into set of examples.

--- X_Data Profiling.py
+++ X_Data Profiling.py
@@ -14,55 +14,11 @@ from azureml.dataprep import Dataflow
 from commonCode import savePackage, openPackage, createFullPackagePath
 
 #%%
-dataFlow = Dataflow.open('./packages/MEMBERS/2/MEMBERS_A_package.dprep')
+dataFlow = Dataflow.open('./packages/PEOPLE/2/PEOPLE_A_package.dprep')
 
 #%%
 dataProfile = dataFlow.get_profile()
 
-#%%
-columnStats = pd.DataFrame(columns = [ \
-    'DataName', \
-    'Stage', \
-    'ColumnName', \
-    'Type', \
-    'Min', \
-    'Max', \
-    'RowCount', \
-    'MissingCount', \
-    'NotMissingCount', \
-    'ErrorCount', \
-    'EmptyCount', \
-    'Mean', \
-    'ValueCount'])
-
-#%%
-for item in dataProfile.columns.values():
-    if item.value_counts == None:
-        valueCount = None
-    else:
-        valueCount = len(item.value_counts)
-    columnStats = columnStats.append({ \
-    'DataName' : 'TABLE', \
-    'Stage' : '02', \
-    'ColumnName' : item.column_name, \
-    'Type' : item.type, \
-    'Min' : item.min, \
-    'Max' : item.max, \
-    'RowCount' : item.count, \
-    'MissingCount' : item.missing_count, \
-    'NotMissingCount' : item.not_missing_count, \
-    'ErrorCount' : item.error_count, \
-    'EmptyCount' : item.empty_count, \
-    'Mean' : item.mean, 'ValueCount' : valueCount}, \
-    ignore_index = True)
-columnStats.insert(2, 'DateTime', datetime.datetime.now())
-
-#%%
-columnStats
-
-#%%
-for c in dataProfile.columns.keys():
-    print(dataProfile.columns[c].value_counts)
 #%%
 dataProfile
 
@@ -70,8 +26,13 @@ dataProfile
 dataColumns = dataProfile.columns.keys()
 
 #%%
-dataProfileValues = dataProfile.columns.values()
+for c in dataColumns:
+    print('Column {0} : value count {1}'.format(c, len(dataProfile.columns[c].value_counts)))
+    print(dataProfile.columns[c].value_counts)
 
+
+#%%
+dataProfileValues = dataProfile.columns.values()
 
 #%%
 columnStats = pd.DataFrame(columns = ['DataName', 'Stage', 'DateTIme', 'ColumnName', 'Type', 'Min', 'Max', 'RowCount', 'MissingCount', 'NotMissingCount', 'ErrorCount', 'EmptyCount', 'Mean'])
@@ -85,7 +46,7 @@ for item in dataProfileValues:
 #%%
 df = dataFlow.to_pandas_dataframe()
 #%%
-dateOfBirth = df[['ID','DOB']]
+dateOfBirth = df[['ID','DOB','TITLE','MTST']]
 
 #%%
 profileReport = pp.ProfileReport(dateOfBirth, check_correlation = True)
